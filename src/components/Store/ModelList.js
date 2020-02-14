d604012464d72a620e69e36982417b6922e13e28
= <=>
2020-02-14 20:03:09 +0100

    Model cards with bg

--- src/components/Store/ModelList.js
+++ src/components/Store/ModelList.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+
 const ModelList = ({models}) => {
+
+    const [cardsBg, setBg] = useState([])
+
+    useEffect(() => {
+        const filesSrc = require.context('../../assets/images/production/cards', false,  /\.(png|jpg|svg)$/)
+        const cardsBg = filesSrc.keys().map(filesSrc)
+        setBg([...cardsBg])
+    }, [])
 
     const renderItems = () => {
         return models.data.map((item, index) => {
             return(
-                <li key = {index}>{item.Name}</li>
+                <>
+                    <div className = "itemBackground col-4" style = {{backgroundImage: `url(${cardsBg[Math.floor(Math.random() * 3)]})`}}>
+                        <div 
+                            key = {index} 
+                            className = "itemContainer" 
+                        >
+                            <span>{item.Name}</span>
+                            <span
+                                className = {item.AvailableCount !== 0 ? "avaliable" : "notAvaliable"}
+                            >{item.AvailableCount !== 0 ? "Есть в наличии" : "Нет в наличии"}</span>
+                        </div>
+                    </div>
+                </>
             )
         })
     }
@@ -24,9 +45,7 @@ const ModelList = ({models}) => {
                     <FontAwesomeIcon icon = "search" />
             </div>
             <div className = "modelContainer row">
-                <ul>
-                    {renderItems()}
-                </ul>
+                {renderItems()}
             </div>  
         </div>
     )
