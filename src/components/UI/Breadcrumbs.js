8ebf8a9c5d84239486b466af84fb22638fdd3fb0
= <=>
2020-02-20 21:57:46 +0100

    Breadcrumbs fixed Brand link

--- src/components/UI/Breadcrumbs.js
+++ src/components/UI/Breadcrumbs.js
@@ -3,7 +3,7 @@ import {Link, useHistory} from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { clearModel } from '../../redux/choseStuff/choseStuffAction'
 
-const Breadcrumbs = ({typeStuff, model,  brand, stuff}) => {
+const Breadcrumbs = ({typeStuff, model, brand, stuff}) => {
 
     const history = useHistory()
 
@@ -11,7 +11,7 @@ const Breadcrumbs = ({typeStuff, model,  brand, stuff}) => {
         <div className = "breadcrumbsContainer">
             <ul id="breadcrumb">
                 <li><Link to={`/production/${typeStuff}`}><span className="icon"> </span>{stuff}</Link></li>
-                {brand ? <li><a href = {null} onClick = {model ? () => {history.goBack()} : null}><span className="icon"></span>{brand}</a></li> : null}
+                {brand ? <li><Link to = {`/production/${typeStuff}/${brand.replace("-","").toLowerCase()}`}><span className="icon"></span>{brand}</Link></li> : null}
                 {model ? <li><a href = {null}><span className="icon"></span>{model.Name.substring(0, 20)} ...</a></li> : null}
             </ul>
         </div>
